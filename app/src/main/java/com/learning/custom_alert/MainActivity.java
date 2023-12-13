8e3cc3040b53ba57f6c70a8fe63c417c7c9e9e59
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-12-13 22:50:02 +0600

    first commit

---
+++ app/src/main/java/com/learning/custom_alert/MainActivity.java
@@ -0,0 +1,37 @@
+package com.learning.custom_alert;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showPopupDialog();
+
+    }
+
+    private void showPopupDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.popup_layout);
+
+        ImageView closeIcon = dialog.findViewById(R.id.closeIcon);
+        closeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+}
